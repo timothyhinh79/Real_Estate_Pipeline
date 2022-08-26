5fdf44e503bb24eddfcadadbd70ad5bf952ed9e5
Tim <timothyhinh79@gmail.com>
2022-08-26 15:39:48 -0700

    Updated weather API codebase to include functionality for retrying the API call if it fails
    Updated API token
    
    Set up proper usage of environmental variables within Weather data ingestion DAG scripts

--- airflow/dags/dag_ingest_weather_data.py
+++ airflow/dags/dag_ingest_weather_data.py
@@ -3,7 +3,7 @@ sys.path.append('/opt/airflow/dags/code/')
 
 import csv
 import os
-from datetime import datetime
+from datetime import datetime, date
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
@@ -11,31 +11,27 @@ from airflow.operators.python import PythonOperator
 from weather_api import *
 from ingest_weather_data_script import ingest_weekly_weather_data
 
-# PG_HOST = os.getenv('POSTGRES_USER')
-# PG_USER = os.getenv('POSTGRES_USER')
-# PG_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-# PG_PORT = os.getenv('PG_PORT')
-# PG_DATABASE = os.getenv('PG_DATABASE')
+PG_HOST = os.getenv('POSTGRES_HOST')
+PG_USER = os.getenv('POSTGRES_USER')
+PG_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+PG_PORT = os.getenv('POSTGRES_PORT')
+PG_DATABASE = os.getenv('POSTGRES_DB')
 
-PG_HOST = 'pgdatabase'
-PG_USER = 'root'
-PG_PASSWORD = 'root'
-PG_PORT = '5432'
-PG_DATABASE = 'weather'
-
-
-with open('/opt/airflow/dags/data/ca-zip-code-list.csv') as csvfile:
+# getting list of all CA zip codes
+with open('/opt/airflow/dags/data/Zip_Codes_(LA_County).csv') as csvfile:
     rows = csv.reader(csvfile)
     next(rows)
     res = zip(*rows)
+    ca_zips = list(res)[1][:5] # list of California zip codes
 
-ca_zips = list(res)[0][:100] # list of California zip codes
+retry_sleep_time = 60
+api_max_attempts = 2
 
 local_workflow = DAG(
     "WeatherIngestionDAG",
-    schedule_interval="0 15 * * 1",
+    schedule_interval="@once", #"0 15 * * 1"
     max_active_runs = 1,
-    start_date=datetime(2022, 7, 25)
+    start_date=datetime(2022, 8, 15)
 )
 
 
@@ -53,6 +49,8 @@ with local_workflow:
             'host': PG_HOST,
             'port': PG_PORT,
             'locations': ca_zips,
+            'retry_sleep_time': retry_sleep_time,
+            'api_max_attempts': api_max_attempts,
         }
     )
 
