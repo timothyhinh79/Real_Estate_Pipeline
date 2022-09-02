9cccab45d83faf9c9c57628a2155c39c8d86a27e
Tim <timothyhinh79@gmail.com>
2022-09-01 17:52:58 -0700

    added weather and crimes summary models, along with schema documentation

--- dbt/dbt-postgres/models/staging/stg_weather_data.sql
+++ dbt/dbt-postgres/models/staging/stg_weather_data.sql
@@ -2,7 +2,10 @@
 {{ config(materialized='view') }}
 
 WITH forecasts_w_rn AS (
-    SELECT rank() OVER (PARTITION BY zip_code, date ORDER BY id DESC) as rn, *
+    SELECT 
+        rank() OVER (PARTITION BY zip_code, date ORDER BY id DESC) as rn
+        , EXTRACT(MONTH FROM date) * 100 + EXTRACT(DAY FROM date) AS month_day
+        , *
     FROM {{ source('staging','daily_forecasts') }}
     WHERE zip_code IS NOT NULL AND date IS NOT NULL
 )
@@ -11,6 +14,11 @@ SELECT
     id
     , zip_code
     , date
+    , CASE WHEN month_day >= 1220 OR month_day < 320 THEN 'WINTER'
+           WHEN month_day >= 320 and month_day < 620 THEN 'SPRING' 
+           WHEN month_day >= 620 and month_day < 920 THEN 'SUMMER' 
+           WHEN month_day >= 920 and month_day < 1220 THEN 'FALL' 
+      END AS season
     , maxtemp_c
     , maxtemp_f
     , mintemp_c
